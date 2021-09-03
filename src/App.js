b3fc38e381bc86fd2f008d37bcb1e6d3d8947bcc
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-09-03 15:16:47 -0700

    updates app.js to simple text, replaces favicon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>delanovation</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          (coming soon)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
